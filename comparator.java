14a69cafc07c9a912766f3e4a34f5818f8c2f673
vtu30298-arch <vtu30298@veltech.edu.in>
2026-09-09 12:11:22 +0530

    Create comparator.java

---
+++ comparator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Checker implements Comparator<Player> {
+    public int compare(Player a, Player b) {
+        if (a.score != b.score) {
+            return b.score - a.score;
+        }
+        return a.name.compareTo(b.name);
+    }
+}
