9fd3aa6e024a7ac377b0093e3a200bef78c5c85b
vtu30298-arch <vtu30298@veltech.edu.in>
2026-09-11 08:54:53 +0530

    Create java arrayList.java

---
+++ java arrayList.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<ArrayList<Integer>> lines = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int d = sc.nextInt();
+            ArrayList<Integer> list = new ArrayList<>();
+
+            for (int j = 0; j < d; j++) {
+                list.add(sc.nextInt());
+            }
+
+            lines.add(list);
+        }
+
+        int q = sc.nextInt();
+
+        for (int i = 0; i < q; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if (x >= 1 && x <= n && y >= 1 && y <= lines.get(x - 1).size()) {
+                System.out.println(lines.get(x - 1).get(y - 1));
+            } else {
+                System.out.println("ERROR!");
+            }
+        }
+
+        sc.close();
+    }
+}
